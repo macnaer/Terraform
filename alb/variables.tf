690cf2cdc54785fb7b564bd9cdc5e1dfe0a88309
macnaer <trofimchuk.an@gmail.com>
2025-12-18 21:43:17 +0200

    Added ALB

--- alb/variables.tf
+++ alb/variables.tf
@@ -1,11 +1,11 @@
 variable "aws_access_key" {
   type    = string
-  default = "AKIAXY7P3NWVLWWOB67Q"
+  default = ""
 }
 
 variable "aws_secret_key" {
   type    = string
-  default = "/Rvi9Z4dXHnPKO9es0BRbgirvyR9BCbSc3jjkxzV"
+  default = ""
 }
 
 variable "region" {
